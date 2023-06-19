39230b64a790300382586810b56a0c93ba933200
CodesofGrace <nwankwogracie@gmail.com>
2023-06-19 16:07:14 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  *main - Entry point
  *Description: Write a program that prints all possible
  *combinations of single-digit numbers.
- *Returns: 0
+ *Return: 0
  */
 
 int main(void)
@@ -11,15 +14,14 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 100; a++)
+	for (a = 0; a < 10; a++)
 	{
-		putchar(a + '0');
-		if (a > 9)
+		putchar(a);
+		if (a != '9')
 		{
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+	}	putchar(' ');
 		}
-	}
 	putchar('\n');
 	return (0);
 }
