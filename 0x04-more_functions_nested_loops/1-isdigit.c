543e58a562673eba2160fb9b6e2cfcaba3f91720
CodesofGrace <nwankwogracie@gmail.com>
2023-06-22 13:48:18 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * _isdigit - Check if a letter is a digit
+ * @a: The number to be checked
+ * Return: 1 if a letter is a digit else return 0
+ */
+
+int _isdigit(int a)
+
+{
+	if (a >= 48 && a <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
