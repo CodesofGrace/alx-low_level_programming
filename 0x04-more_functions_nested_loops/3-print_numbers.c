a79cb8f9a212c1d9206ba3f85354d1ab4d76697a
CodesofGrace <nwankwogracie@gmail.com>
2023-06-22 14:11:52 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - Print from 0 to 9
+ *Return: The numbers from 0 to 9
+ */
+
+void print_numbers(void)
+
+{
+	int g;
+
+	for (g = 0; g <= 9; g++)
+	{
+		_putchar(g * '0');
+	}
+	_putchar('/n');
+}
