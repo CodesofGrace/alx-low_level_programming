961a2ea8dbc2ba48b23bc64b83a71fa185428bfe
CodesofGrace <nwankwogracie@gmail.com>
2023-06-22 13:22:45 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *__isupper - check if a letteris upper
- *@x: The number to be checked
+ *_isupper - check if a letteris upper
+ *@c: The number to be checked
  *Return: 1 for upper leter or 0 for anything else
  */
 
