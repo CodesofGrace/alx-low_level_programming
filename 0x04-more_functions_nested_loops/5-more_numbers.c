8605cc16b3a0a15d7e953898d3ac28c16c0315e2
CodesofGrace <nwankwogracie@gmail.com>
2023-06-23 03:46:01 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times number 0 to 14
+ * Return: 10 times of the numbers 0 to 14
+ */
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	more_numbers();
+	return (0);
+}
