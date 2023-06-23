82f94522400bdf6fe9366e6f739b2d16f73c288f
CodesofGrace <nwankwogracie@gmail.com>
2023-06-22 17:01:29 -0700

    3-print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *print_numbers - Print from 0 to 9
- *Return: The numbers from 0 to 9
+ * print_numbers - Print from 0 to 9
+ * Return: The numbers from 0 to 9
  */
 
 void print_numbers(void)
