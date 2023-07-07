f5b08124844485c4ce20bd1770cd94101ae058d7
CodesofGrace <nwankwogracie@gmail.com>
2023-07-07 14:16:31 -0700

    Static Library is fun

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+	/* Function implementation */
+}
+
+/**
+ * main - Entry point of the program.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Function implementation */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
