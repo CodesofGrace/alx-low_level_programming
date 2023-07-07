fe0283349f05d12b08a367e0d1e51139b7dad9a7
CodesofGrace <nwankwogracie@gmail.com>
2023-07-07 16:45:24 -0700

    My name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always zero
+ */
+
+int main(int argc __attribute__((__unused__)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
