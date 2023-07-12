2f2fee2a22dd21a09a9e2210bddfafecb2876705
CodesofGrace <nwankwogracie@gmail.com>
2023-07-11 17:43:23 -0700

    Create-array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicates a string
+ * @str: string to be duplicate
+ * Return: the duplicated string
+ */
+
+char *_strdup(char *str)
+
