28050d01337f18170524039792b4ca113f6e8829
CodesofGrace <nwankwogracie@gmail.com>
2023-08-01 15:42:21 -0700

    Print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 
 /**
- * print_listint - All document in a linked list is to be printed
- * @h: linked list of type listint_t to print
+ * print_listint - Prints all elements in a linked list when run
+ * @h: Linked list of type listint_t to print.
  *
- * Return: number of nodes found
+ * Return: Number of nodes found.
  */
 size_t print_listint(const listint_t *h)
 {
-	if (h == NULL) {
-		return 0;
-	}
+        if (h == NULL)
+        {
+                return (0);
+        }
 
-	printf("%d\n", h->n);
-	return 1 + print_listint(h->next);
+        printf("%d\n", h->n);
+        return (1 + print_listint(h->next));
 }
