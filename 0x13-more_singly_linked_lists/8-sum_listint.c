580fa8ee5cef9983104d30880eaf14665e8681a7
CodesofGrace <nwankwogracie@gmail.com>
2023-08-01 17:28:55 -0700

    Sum numbers

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Calculates the sum of all info in a listint_t list.
+ * @head: First node in the linked list.
+ *
+ * Return: Resulting sum.
+ */
+int sum_listint(listint_t *head)
+{
+	if (head == NULL)
+		return 0;
+
+	return head->n + sum_listint(head->next);
+}
+
